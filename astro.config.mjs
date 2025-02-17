655b0231d675a17ff69ad2d81dc46415a4831320
Nick Breaton <nick@nickbreaton.com>
2025-02-17 15:05:19 -0500

    Add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import solidJs from "@astrojs/solid-js";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs()],
@@ -12,4 +14,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
